d0dc51454be1792e8f690a1fa12438c801221406
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2021-05-09 00:55:18 +0200

    init

---
+++ lib/compound_interest.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "compound_interest/version"
+
+module CompoundInterest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/compound_interest_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CompoundInterest do
+  it "has a version number" do
+    expect(CompoundInterest::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
