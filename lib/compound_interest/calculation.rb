a5d619921f799288ac5775089a7fed9ef2db0c3b
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2021-05-11 15:35:02 +0200

    refactor

--- lib/compound_interest/calculation.rb
+++ lib/compound_interest/calculation.rb
@@ -1,39 +1,50 @@
+# frozen_string_literal: true
+
 module CompoundInterest
   class Calculation
     def self.calculate(params)
-      raise "Argument must be Hash" if params.class != Hash
-      if params[:initial_payment].nil?
-        raise ArgumentError, "Argument must include :initial_payment"
-      elsif params[:term].nil?
-        raise ArgumentError, "Argument must include :term"
-      elsif params[:interest_rate].nil?
-        raise ArgumentError, "Argument must include :interest_rate"
-      elsif [:capitalization_periodicity].nil?
-        raise ArgumentError, "Argument must include :capitalization_periodicity"
-      end
+      raise_error(params)
 
-      initial_payment = params[:initial_payment]
+      @initial_payment = params[:initial_payment]
       # number of terms
-      term = params[:term]
-      # ineterest rate per term
-      interest_rate = params[:interest_rate].to_f / 100
+      @term = params[:term]
+      # interest rate per term
+      @interest_rate = params[:interest_rate].to_f / 100
       # number of interest payments for the term
-      capitalization_periodicity = params[:capitalization_periodicity]
-      payment = params[:payment] || 0
+      @capitalization_periodicity = params[:capitalization_periodicity]
+      @payment = params[:payment] || 0
       # number of payments for the term
-      payment_periodicity = params[:payment_periodicity]
+      @payment_periodicity = params[:payment_periodicity]
 
-      calculation = (1.0 + interest_rate / capitalization_periodicity)**(term * capitalization_periodicity)
-      result = initial_payment * calculation
-      if payment > 0
-        times = (payment_periodicity * term).to_i
-        payment_result = 0
-        (1..times).each do |i|
-          payment_result += payment * calculation - ((capitalization_periodicity / payment_periodicity) * i)
-        end
-        result = result + payment_result - payment
-      end
       result
+    end
+
+    def self.result
+      result = @initial_payment * compound_interest_formula
+      result = result + periodical_payment_calculation - @payment if @payment.positive?
+      result
+    end
+
+    def self.periodical_payment_calculation
+      times = (@payment_periodicity * @term).to_i
+      payment_result = 0
+      (1..times).each do |i|
+        payment_result += @payment * compound_interest_formula -
+                          ((@capitalization_periodicity / @payment_periodicity) * i)
+      end
+      payment_result
+    end
+
+    def self.compound_interest_formula
+      (1.0 + @interest_rate / @capitalization_periodicity)**(@term * @capitalization_periodicity)
+    end
+
+    def self.raise_error(params)
+      raise "Argument must be Hash" if params.class != Hash
+      raise ArgumentError, "Argument must include :initial_payment" if params[:initial_payment].nil?
+      raise ArgumentError, "Argument must include :term" if params[:term].nil?
+      raise ArgumentError, "Argument must include :interest_rate" if params[:interest_rate].nil?
+      raise ArgumentError, "Argument must include :capitalization_periodicity" if [:capitalization_periodicity].nil?
     end
   end
 end
